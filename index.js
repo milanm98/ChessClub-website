f2aad8320226e64157a80edbcdc75de73db3ff3a
milanm98 <marjanovmilan13@gmail.com>
2021-06-18 15:04:27 +0200

    sass structure

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $( "#menu-mobile" ).click(function() {
     $( ".nav-bar-links" ).css({"display":"flex" , "background-color":"#185c94"});
-    $("nav").css("background-color","#185c94");
+    $("nav-bar").css("background-color","#185c94");
     $("#menu-mobile").css("display", "none");
     $("#close-menu-mobile").css("display","block");
   });
@@ -9,5 +9,5 @@ $( "#close-menu-mobile" ).click(function() {
     $( ".nav-bar-links" ).css("display","none");
     $("#menu-mobile").css("display", "block");
     $("#close-menu-mobile").css("display","none");
-    $("nav").css("background-color","#185c94");
+    $("nav-bar").css("background-color","#185c94");
   });
