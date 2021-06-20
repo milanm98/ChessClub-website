f6159d5735500ed1e85d2bdce652cc6ae6146dfd
milanm98 <marjanovmilan13@gmail.com>
2021-06-21 01:02:48 +0200

    margin fixes

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 $( "#menu-mobile" ).click(function() {
-    $( ".nav-bar-links" ).css({"display":"flex" , "background-color":"#185c94", "animation" : "puls 2s normal"});
+    $( ".nav-bar-links" ).css({"display":"flex" , "background-color":"#185c94", "animation" : "puls 1s normal"});
     $("nav-bar").css("background-color","#185c94");
     $("#menu-mobile").css("display", "none");
     $("#close-menu-mobile").css("display","block");
-    
   });
 
 $( "#close-menu-mobile" ).click(function() {
-    $( ".nav-bar-links" ).css({"display" :"none", "animation" : "puls 2s reverse"});
+    $( ".nav-bar-links" ).css({"display" :"none", "animation" : "puls 1s reverse"});
     $("#menu-mobile").css("display", "block");
     $("#close-menu-mobile").css("display","none");
     $("nav-bar").css("background-color","#185c94");
